07dc291df0b1d62afbdcde7f4ad14f5056ad312e
Grace Yang <yaycake@gmail.com>
2017-11-28 10:50:50 +0800

    completed meet & meat controller

--- app/controllers/meat_controller.rb
+++ app/controllers/meat_controller.rb
@@ -1,13 +1,16 @@
 class MeatController < ApplicationController
+  before_action :set_meat, only: [:update, :edit, :show]
+
   def index
-    @meaters = Meater.where(user_category: "meat")
+    @meats = Meater.where(user_category: "meat")
   end
 
-  def new
-    @meater.user_category = "Meat"
-  end
-
-  def create
+  def update
+    @meat.user_category = "Meat"
+    @meat.update(meat_parameters)
+    #collect id of current meater (set_meat)
+    # change user category status to 'meat'
+    # add the rest of the attributes
   end
 
   def edit
@@ -19,9 +22,11 @@ class MeatController < ApplicationController
   private
 
   def set_meat
+    @meat = Meater.find(param[:id])
   end
 
   def meat_parameters
+    param.require(:meater).permit(:gender, :summary, :description, :hobbies, :education, :nationality, :height, :weight, :occupation, :location, :price)
   end
 
 end

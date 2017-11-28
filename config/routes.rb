16a9ac38dc8f0500559204e1da70e75f763b7c00
Kabirova Alina <kabirova.alina@icloud.com>
2017-11-28 18:35:59 +0800

    Added booking view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :meaters
 
   resources :meeters, only: [:show, :create]
-  resources :meats, only: [:index, :update, :show]
+  resources :meats, only: [:index, :update, :show] do
+    resources :bookings, only: [:new]
+  end
+
   resources :bookings, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

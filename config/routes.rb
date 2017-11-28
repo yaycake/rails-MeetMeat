468e05bd9373236bb1531787ec56a1660a6a1041
Fabien Martin <fabien.martin87@gmail.com>
2017-11-28 13:05:40 +0800

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :meaters
+
+  resources :meeters, only: [:show, :create]
+  resources :meats, only: [:index, :update, :show]
+  resources :bookings, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
